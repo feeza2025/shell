abad1dbf0f326def3ba8b17b4345e7178470689f
Shafeeza Hussain <shafeezahussain@gmail.com>
2025-11-08 10:48:42 -0500

    Listing the files in data/processed recursively

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,7 +52,7 @@ cp ./data/raw/*event* ./data/processed/event_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls ./data/processed > ./data/inventory.txt
+ls -R ./data/processed > ./data/inventory.txt
 
 ###########################################
 
